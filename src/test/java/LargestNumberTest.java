792f50394f18da24b3927a2e90226e22bb515b13
ZeroJL <youngjae991@gmail.com>
2022-07-12 23:36:33 +0900

    [Test] add making the largest number test 2

--- src/test/java/LargestNumberTest.java
+++ src/test/java/LargestNumberTest.java
@@ -11,4 +11,13 @@ public class LargestNumberTest {
 
         Assertions.assertEquals("210", result);
     }
+
+    @Test
+    void solutionTest2() {
+        int[] nums = new int[]{3, 30, 34, 5, 9};
+        LargestNumber largestNumber = new LargestNumber();
+        String result = largestNumber.solution(nums);
+
+        Assertions.assertEquals("9534330", result);
+    }
 }
