29f8cbae2142fc2876a3e3e487fae3faef4a0aa5
ZeroJL <youngjae991@gmail.com>
2022-07-12 23:44:23 +0900

    [Feat] solution for making the largest number using sort

--- src/main/java/LargestNumber.java
+++ src/main/java/LargestNumber.java
@@ -1,6 +1,22 @@
+import java.util.Arrays;
+
 public class LargestNumber {
 
     public String solution(int[] nums) {
-        return "210";
+        String[] numsToString = new String[nums.length];
+        for (int i = 0; i < nums.length; ++i) {
+            numsToString[i] = Integer.toString(nums[i]);
+        }
+
+        Arrays.sort(numsToString, (s1, s2) -> (s2 + s1).compareTo(s1 + s2));
+        if ("0".equals(numsToString[0])) {
+            return "0";
+        }
+        String result = "";
+        for (String str : numsToString) {
+            result += str;
+        }
+
+        return result;
     }
 }
