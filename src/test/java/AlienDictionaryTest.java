514e7e225edfa6664cba7fd03551daedfbd793cb
ZeroJL <youngjae991@gmail.com>
2022-07-15 23:15:42 +0900

    [Test] add another test for alien dictionary

--- src/test/java/AlienDictionaryTest.java
+++ src/test/java/AlienDictionaryTest.java
@@ -10,4 +10,20 @@ public class AlienDictionaryTest {
         String result = alienDictionary.solution(words);
         Assertions.assertEquals("wertf", result);
     }
+
+    @Test
+    void solution2() {
+        String[] words = {"z", "x"};
+        AlienDictionary alienDictionary = new AlienDictionary();
+        String result = alienDictionary.solution(words);
+        Assertions.assertEquals("zx", result);
+    }
+
+    @Test
+    void solution3() {
+        String[] words = {"z", "x", "z"};
+        AlienDictionary alienDictionary = new AlienDictionary();
+        String result = alienDictionary.solution(words);
+        Assertions.assertEquals("", result);
+    }
 }
