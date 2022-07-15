9f85c5854d9f596507089ec3999a57e75abf957f
ZeroJL <youngjae991@gmail.com>
2022-07-15 23:11:05 +0900

    [Test] make test for alien dictionary

---
+++ src/test/java/AlienDictionaryTest.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class AlienDictionaryTest {
+
+    @Test
+    void solution1() {
+        String[] words = {"wrt", "wrf", "er", "ett", "rftt"};
+        AlienDictionary alienDictionary = new AlienDictionary();
+        String result = alienDictionary.solution(words);
+        Assertions.assertEquals("wertf", result);
+    }
+}
