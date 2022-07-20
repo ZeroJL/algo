e8b9d68cac1a76208676151aaa6e3c7418b8acc5
ZeroJL <youngjae991@gmail.com>
2022-07-20 23:25:12 +0900

    [Feat] add solution for find minimum meeting room

--- src/main/java/MeetingRoomsII.java
+++ src/main/java/MeetingRoomsII.java
@@ -1,6 +1,23 @@
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
 public class MeetingRoomsII {
     public int solution(int[][] intervals) {
+        Arrays.sort(intervals, Comparator.comparingInt(a -> a[0]));
 
-        return 2;
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>(Comparator.comparingInt(a -> intervals[a][1]));
+        minHeap.add(0);
+
+        int result = 1;
+        for (int i = 1; i < intervals.length; ++i) {
+            if (!minHeap.isEmpty() && intervals[i][0] >= intervals[minHeap.peek()][1]) {
+                minHeap.poll();
+            }
+            minHeap.add(i);
+            result = Math.max(result, minHeap.size());
+        }
+
+        return result;
     }
 }
