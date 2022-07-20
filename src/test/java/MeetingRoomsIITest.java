f02beeecac6a6a0456102283c598c37d2b450bde
ZeroJL <youngjae991@gmail.com>
2022-07-20 23:12:56 +0900

    [Test] add another test

--- src/test/java/MeetingRoomsIITest.java
+++ src/test/java/MeetingRoomsIITest.java
@@ -11,4 +11,13 @@ public class MeetingRoomsIITest {
 
         Assertions.assertEquals(2, result);
     }
+
+    @Test
+    void solution2() {
+        int[][] intervals = {{7, 10}, {2, 4}};
+        MeetingRoomsII meetingRoomsII = new MeetingRoomsII();
+        int result = meetingRoomsII.solution(intervals);
+
+        Assertions.assertEquals(1, result);
+    }
 }
