efb6c274ee1cb90ac6a39b2245b2ac660bba992c
ZeroJL <youngjae991@gmail.com>
2022-07-21 23:52:02 +0900

    [Feat] add solution for find missing number

--- src/main/java/MissingNumber.java
+++ src/main/java/MissingNumber.java
@@ -1,6 +1,20 @@
+import java.util.HashSet;
+import java.util.Set;
+
 public class MissingNumber {
     public int solution(int[] nums) {
+        Set<Integer> set = new HashSet<>();
 
-        return 2;
+        for (int num : nums) {
+            set.add(num);
+        }
+
+        for (int i = 0; i <= nums.length; ++i) {
+            if (!set.contains(i)) {
+                return i;
+            }
+        }
+
+        return -1;
     }
 }
