2309d8a1902374ef6b0e2f36446e55439cf4fe62
ZeroJL <youngjae991@gmail.com>
2022-07-21 23:54:09 +0900

    [Feat] change solution using O(1) space

--- src/main/java/MissingNumber.java
+++ src/main/java/MissingNumber.java
@@ -3,18 +3,12 @@ import java.util.Set;
 
 public class MissingNumber {
     public int solution(int[] nums) {
-        Set<Integer> set = new HashSet<>();
+        int missing = nums.length;
 
-        for (int num : nums) {
-            set.add(num);
+        for (int i = 0; i < nums.length; ++i) {
+            missing ^= nums[i] ^ i;
         }
 
-        for (int i = 0; i <= nums.length; ++i) {
-            if (!set.contains(i)) {
-                return i;
-            }
-        }
-
-        return -1;
+        return missing;
     }
 }
