2c4bcab774f035b6aaef7409deab76fd0c01f434
ZeroJL <youngjae991@gmail.com>
2022-07-22 23:42:31 +0900

    [Feat] add solution for find palindrome number

--- src/main/java/PalinDromeNumber.java
+++ src/main/java/PalinDromeNumber.java
@@ -1,6 +1,17 @@
 public class PalinDromeNumber {
     public boolean solution(int x) {
+        if (x < 0 || x % 10 == 0 && x != 0) {
+            return false;
+        }
 
-        return true;
+        int reversedNum = 0;
+        int current = x;
+
+        while (current > reversedNum) {
+            reversedNum = reversedNum * 10 + current % 10;
+            current /= 10;
+        }
+
+        return current == reversedNum || current == reversedNum / 10;
     }
 }
