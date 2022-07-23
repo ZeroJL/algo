c95f874402754e32140fccaa1676826908d0b0da
ZeroJL <youngjae991@gmail.com>
2022-07-23 14:33:57 +0900

    [Feat] add solution for find max num in sliding window

--- src/main/java/SlidingWindowMaximum.java
+++ src/main/java/SlidingWindowMaximum.java
@@ -1,6 +1,33 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 public class SlidingWindowMaximum {
     public int[] solution(int[] nums, int k) {
 
-        return new int[]{3, 3, 5, 5, 6, 7};
+        if (nums == null || k == 0) {
+            return new int[0];
+        }
+
+        int[] result = new int[nums.length - k + 1];
+        int resultIndex = 0;
+
+        Deque<Integer> queue = new ArrayDeque<>();
+        for (int i = 0; i < nums.length; ++i) {
+            if (!queue.isEmpty() && queue.size() > k - 1) {
+                queue.poll();
+            }
+
+            while (!queue.isEmpty() && nums[queue.peekLast()] < nums[i]) {
+                queue.poll();
+            }
+
+            queue.add(i);
+            if (i >= k - 1) {
+                result[resultIndex++] = nums[queue.peek()];
+            }
+        }
+
+
+        return result;
     }
 }
