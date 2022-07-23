6da0b779380b550825ab3d38ffee173851cbb624
ZeroJL <youngjae991@gmail.com>
2022-07-23 14:35:14 +0900

    [Test] add another test for find max num in sliding window

--- src/test/java/SlidingWindowMaximumTest.java
+++ src/test/java/SlidingWindowMaximumTest.java
@@ -23,4 +23,14 @@ public class SlidingWindowMaximumTest {
 
         Assertions.assertArrayEquals(new int[]{1}, result);
     }
+
+    @Test
+    void solution3() {
+        int[] nums = {1, 3, 1, 2, 0, 5};
+        int k = 3;
+        SlidingWindowMaximum slidingWindowMaximum = new SlidingWindowMaximum();
+        int[] result = slidingWindowMaximum.solution(nums, k);
+
+        Assertions.assertArrayEquals(new int[]{3, 3, 2, 5}, result);
+    }
 }
